3ddc41b54edeed2712e8179105fe4d19ffe1d88c
Rishik Sinha <kumarrishiksinha620@gmail.com>
2025-09-28 04:59:33 +0530

    Configure project for manual deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HiNEST-Care/', // This line is crucial
+  base: '/hinestcare/',
   plugins: [react()],
 })
