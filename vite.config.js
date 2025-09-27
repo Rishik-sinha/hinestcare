a404c71cf201e9a3d8205c8b2fe41104d90d50db
Rishik Sinha <kumarrishiksinha620@gmail.com>
2025-09-28 04:45:41 +0530

    feat: Add new sections and update styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/HiNEST-Care/', // Use your repository name here
+  base: '/HiNEST-Care/', // This line is crucial
   plugins: [react()],
 })
